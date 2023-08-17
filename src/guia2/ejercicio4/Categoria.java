7aab7fee1d8d1dd37e8c5ba28bc3aaa4489a1a29
kerlinFranco <kerlinfranco@gmail.com>
2023-08-17 20:55:14 -0300

    Initial commit

---
+++ src/guia2/ejercicio4/Categoria.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guia2.ejercicio4;
+
+/**
+ *
+ * @author josel
+ */
+public enum Categoria {
+    
+    COMESTIBLE,
+    LIMPIEZA,
+    PERFUMERIA,
+    
+}
